983dcfc7c4330b0927c613a9e827645bbd80b7ae
Kanta-Haruna <82809366+Kanta-Haruna@users.noreply.github.com>
2024-06-17 17:01:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import pandas as pd
 import scray
 import plot
 
-csvfile = '/Users/harunakanta/PythonTraining/csv/chimosy.csv'
+csvfile = st.file_uploader("CSVファイルをアップロードしてください", type=["csv"])
 st.set_option('deprecation.showPyplotGlobalUse', False)
+
 
 st.title('チモシー価格表')
 md = """
@@ -17,19 +18,18 @@ md = """
 """
 st.markdown(md)
 
-df = pd.read_csv(csvfile, encoding='shift-jis')
+if csvfile is not None:
+    df = pd.read_csv(csvfile, encoding='shift-jis')
+    st.dataframe(df, column_config={
+        "URL": st.column_config.LinkColumn(
+                "URL", 
+                display_text="Open Site"
+            )})
+    st.pyplot(plot.plot())
+else:
+    st.write('ファイルをアップロードしてください')
 
-st.dataframe(df, column_config={
-    "URL": st.column_config.LinkColumn(
-            "URL", 
-            display_text="Open Site"
-        )})
-
-
-st.pyplot(plot.plot())
-
-
-
+    
 if st.button('click'):
     scray.makedf()
 else:
